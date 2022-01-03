4718fd94077d57eef1595822faa555394d30dcd7
Sayed Ali Kumail <enggkumail@gmail.com>
2022-01-03 16:37:29 +0400

    Setting Nodemon

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello web 2.0");
-})
+app.use(bodyParser.json());
 
+app.post('/',(req,res) => {
+
+    var email = req.body.email;
+    var  amount = req.body.amount;
+
+    res.send({"amount": amount , "email" : email});
+
+});
 app.listen(3000,()=>{
     console.log('server listening on port 3000');
 });

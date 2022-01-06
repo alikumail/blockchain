2c416e26fd55ac9729bc72c69aa519199d0a99ce
Sayed Ali Kumail <enggkumail@gmail.com>
2022-01-06 16:32:23 +0400

    Axios Post

---
+++ public/index.js
@@ -0,0 +1,41 @@
+class App extends React.Component {
+    constructor(probs){
+        super(probs);
+        this.state = {
+            'total_amount': 1000,
+            'amount': 100,
+            'email': ''
+        }
+    }
+
+    onSubmit = async(event) => {
+     event.preventDefault();
+     const responce = await axios.post('/post_info', {'amount':this.state.amount,'email':this.state.email});
+    }
+render() {
+   return (
+       <div>
+           <h1> HELLO WORLD</h1>
+           <div> 
+               <p>Total Lootery Amount is {this.state.total_amount}</p>
+           </div>
+           <form onSubmit={this.onSubmit}> 
+              <input type="text" placeholder="Amount" value = {this.state.amount} 
+              onChange = {event => this.setState({amount: event.target.value}) } />
+
+              <input type="text" placeholder="email" value = {this.state.email}
+              onChange = {event => this.setState({email: event.target.value}) }/>
+             
+               <button type="submit">Participate</button>
+           </form>
+       </div>
+   ) 
+}
+
+};
+
+ReactDOM.render(
+<div>
+    <App />
+</div> , document.getElementById('reactBinding')
+);
